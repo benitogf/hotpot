1323e3701c2f6427bb3f7bf456a107b7449cc7e4
Benito Gomez <Benito Gomez>
2016-08-31 20:11:37 +0800

    hint fix

--- client/src/index.js
+++ client/src/index.js
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var _ = require('lodash')
 var app = {
   // Application Constructor
   initialize: function () {
