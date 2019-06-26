5db5848a827bb8b0fd6c6a3bf6769e035c7957e4
benitogf <benitogf@outlook.com>
2019-06-26 15:49:48 +0800

    rm spec build

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const replace = require('replacestream')
 const compression = require('compression')
 const io = require('socket.io')(server)
 
-function initServer(conf, log) {
+function initServer (conf, log) {
   app.set('view engine', 'pug')
   app.engine('pug', require('pug').renderFile)
   app.use(compression({ threshold: 0 }))
@@ -61,7 +61,7 @@ function initServer(conf, log) {
   })
 }
 
-function BrowserifyLivereload() {
+function BrowserifyLivereload () {
   let b = this
   let outfile = arguments[0]
   let conf = arguments[1]
